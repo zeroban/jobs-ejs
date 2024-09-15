6240c136fe96a06833863675c072aae2d5ed2f4a
zeroban <theemptyspamfolder@gmail.com>
2024-09-15 11:56:24 -0400

    week12 assignement

--- app.js
+++ app.js
@@ -3,18 +3,84 @@ require("express-async-errors");
 
 const app = express();
 
+// .env update
+require("dotenv").config(); // to load the .env file into the process.env object
+const session = require("express-session");
+
+
+
+
+// updating mongoDB
+const MongoDBStore = require("connect-mongodb-session")(session);
+const url = process.env.MONGO_URI;
+
+const store = new MongoDBStore({
+    // may throw an error, which won't be caught
+    uri: url,
+    collection: "mySessions",
+});
+store.on("error", function (error) {
+    console.log(error);
+});
+
+const sessionParms = {
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: store,
+    cookie: { secure: false, sameSite: "strict" },
+};
+
+if (app.get("env") === "production") {
+    app.set("trust proxy", 1); // trust first proxy
+    sessionParms.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sessionParms));
+
+// added flash
+app.use(require("connect-flash")());
+
+
+
+
+
 app.set("view engine", "ejs");
 app.use(require("body-parser").urlencoded({ extended: true }));
 
+
+
+
+
+
+
+
 // secret word handling
-let secretWord = "syzygy";
+// let secretWord = "syzygy"; <-- comment this out or remove this line
 app.get("/secretWord", (req, res) => {
-    res.render("secretWord", { secretWord });
+    if (!req.session.secretWord) {
+        req.session.secretWord = "syzygy";
+    }
+    res.locals.info = req.flash("info");
+    res.locals.errors = req.flash("error");
+    res.render("secretWord", { secretWord: req.session.secretWord });
 });
+
+
+// updated post request for secretWord
 app.post("/secretWord", (req, res) => {
-    secretWord = req.body.secretWord;
+    if (req.body.secretWord.toUpperCase()[0] == "P") {
+        req.flash("error", "That word won't work!");
+        req.flash("error", "You can't use words that start with p.");
+    } else {
+        req.session.secretWord = req.body.secretWord;
+        req.flash("info", "The secret word was changed.");
+    }
     res.redirect("/secretWord");
 });
+
+
+
 
 app.use((req, res) => {
     res.status(404).send(`That page (${req.url}) was not found.`);
